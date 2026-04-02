f9938a031854d0b361df46dc8accc4710b6eac53
Rifat-instrumental <rifatrifu1323@gmail.com>
2026-04-02 15:23:33 +0900

    Add files via upload

---
+++ format.py
@@ -0,0 +1,60 @@
+##a = 132
+##b = 45
+##fmt0 = '{:<10}'
+##fmt1 = 'Ob{:08b} 0x{:02x}{:3}'
+##n = 30
+##print("bitwise AND:")
+##print(fmt0.format('a'), fmt1.format(a,a,a))
+##print(fmt0.format('b'), fmt1.format(b,b,b))
+##print('-' *n)
+##print(fmt0.format('a&b'),fmt1.format(a&b,a&b,a&b))
+
+##a = 132
+##b = 45
+##fmt0 = '{:<10}'
+##fmt1 = 'Ob{:08b} 0x{:02x}{:3}'
+##n = 30
+##print("bitwise OR:")
+##print(fmt0.format('a'), fmt1.format(a,a,a))
+##print(fmt0.format('b'), fmt1.format(b,b,b))
+##print('-' *n)
+##print(fmt0.format('a|b'),fmt1.format(a|b,a|b,a|b))
+##
+
+
+##a = 132
+##b = 45
+##fmt0 = '{:<10}'
+##fmt1 = 'Ob{:08b} 0x{:02x}{:3}'
+##n = 30
+##print("bitwise XOR:")
+##print(fmt0.format('a'), fmt1.format(a,a,a))
+##print(fmt0.format('b'), fmt1.format(b,b,b))
+##print('-' *n)
+##print(fmt0.format('a^b'),fmt1.format(a^b,a^b,a^b))
+##
+
+
+##a = 132
+##b = 45
+##fmt0 = '{:<10}'
+##fmt1 = 'Ob{:08b} 0x{:02x}{:3}'
+##n = 30
+##print("bitwise NOT:")
+##print(fmt0.format('a'), fmt1.format(a,a,a))
+###print(fmt0.format('b'), fmt1.format(b,b,b))
+##print('-' *n)
+##print(fmt0.format('~a'),fmt1.format(~a&0xFF,~a&0xFF,~a&0xFF))
+
+
+a = 132
+b = 45
+fmt0 = '{:<10}'
+fmt1 = 'Ob{:08b} 0x{:02x}{:3}'
+n = 30
+print("bitwise NOT:")
+print(fmt0.format('a'), fmt1.format(a,a,a))
+#print(fmt0.format('b'), fmt1.format(b,b,b))
+print('-' *n)
+print(fmt0.format('a<<2'),fmt1.format(a<<2&0xFF,a<<2&0xFF,a<<2&0xFF))
+
